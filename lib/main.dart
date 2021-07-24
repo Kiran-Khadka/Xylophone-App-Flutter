84b1842481bec48f725147b540e32c735db73f36
Kiran Khadka <khadka.kiran1110@gmail.com>
2021-07-24 12:32:30 +0545

    function play added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void playSound(int soundNum) {
+    // function to play sound
+    final player = AudioPlayer();
+    player
+        .play('assets/sounds/note$soundNum.wav'); // passing soundNum parameter
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,79 +23,104 @@ class MyApp extends StatelessWidget {
           title: Text("Xylophone App"),
         ),
         body: SafeArea(
-          child: Container(
-            color: Colors.black,
-            width: 200.0,
-            height: 200.0,
+          child: Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play('assets/sounds/note1.wav');
-                  },
-                  child: Text(
-                    "Note 1",
-                    style: TextStyle(
-                      color: Colors.red,
+                Container(
+                  color: Colors.red,
+                  width: 500.0,
+                  height: 50.0,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(1);
+                    },
+                    child: Text(
+                      "Note 1",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play('assets/sounds/note2.wav');
-                  },
-                  child: Text(
-                    "Note 2",
-                    style: TextStyle(
-                      color: Colors.orange,
+                Container(
+                  color: Colors.orange,
+                  width: 500.0,
+                  height: 50.0,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: Text(
+                      "Note 2",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play('assets/sounds/note3.wav');
-                  },
-                  child: Text(
-                    "Note 3",
-                    style: TextStyle(
-                      color: Colors.green,
+                Container(
+                  color: Colors.green,
+                  width: 500.0,
+                  height: 50.0,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: Text(
+                      "Note 3",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play('assets/sounds/note4.wav');
-                  },
-                  child: Text(
-                    "Note 4",
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play('assets/sounds/note5.wav');
-                  },
-                  child: Text(
-                    "Note 5",
-                    style: TextStyle(
-                      color: Colors.deepPurple,
+                Container(
+                  color: Colors.blue,
+                  width: 500.0,
+                  height: 50.0,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: Text(
+                      "Note 4",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play('assets/sounds/note6.wav');
-                  },
-                  child: Text(
-                    "Note 6",
-                    style: TextStyle(
-                      color: Colors.brown,
+                Container(
+                  color: Colors.deepPurple,
+                  width: 500.0,
+                  height: 50.0,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: Text(
+                      "Note 5",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.brown,
+                  width: 500.0,
+                  height: 50.0,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(
+                          6); // passing integer value for soundNum parameter
+                    },
+                    child: Text(
+                      "Note 6",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
